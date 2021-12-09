48d5566e82d08d643da57d051a2f6057b09885b7
Brian Stiner <Brian.Stiner@revature.net>
2021-12-09 14:28:58 -0800

    internal organ boxes hoverable but not clickable

--- Frontend/CloudCure-Angular/src/app/body-clicker/body-clicker.component.ts
+++ Frontend/CloudCure-Angular/src/app/body-clicker/body-clicker.component.ts
@@ -1,15 +1,128 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'app-body-clicker',
+  selector: 'body-clicker',
   templateUrl: './body-clicker.component.html',
-  styleUrls: ['./body-clicker.component.css']
+  styleUrls: ['./body-clicker.component.scss']
 })
+
 export class BodyClickerComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  canEdit: boolean
+
+  @Output('onClick')
+  onClick: EventEmitter<Clickable> = new EventEmitter();
+
+  
+
+  bodyparts: Clickable[] = [
+    {
+      name: 'Right Lung',
+      x: 82,
+      y: 111,
+      width: 28,
+      height: 62
+    },{
+      name: 'Left Lung',
+      x: 122,
+      y: 107,
+      width: 28,
+      height: 62
+    },{
+      name: 'Throat',
+      x: 107,
+      y: 66,
+      width: 20,
+      height: 20
+    },{
+      name: 'Esophagus',
+      x: 112,
+      y: 82,
+      width: 9,
+      height: 36
+    },{
+      name: 'Diaphragm',
+      x: 88,
+      y: 169,
+      width: 58,
+      height: 17
+    },{
+      name: 'Liver',
+      x: 85,
+      y: 190,
+      width: 40,
+      height: 20
+    },{
+      name: 'Stomach',
+      x: 129,
+      y: 190,
+      width: 24,
+      height: 32
+    },{
+      name: 'Large Intestine',
+      x: 82,
+      y: 237,
+      width: 71,
+      height: 52
+    },{
+      name: 'Small Intestine',
+      x: 96,
+      y: 250,
+      width: 45,
+      height: 33
+    }
+  ]
+
+  constructor(){
+    this.canEdit = false;
+  }
 
   ngOnInit(): void {
   }
 
+
+  getBodyPartStyle(bodypart: Clickable): object {
+    return {
+      top: `${bodypart.y}px`,
+      left: `${bodypart.x}px`,
+      height: `${bodypart.height}px`,
+      width: `${bodypart.width}px`
+    };
+  }
+
+  onAreaClick(bodypart: Clickable) {
+    this.onClick.emit(bodypart);
+  }
+
+  onAreaContext(e: MouseEvent, bodypart: Clickable) {
+    if(bodypart){console.log('editing')}
+    else{console.log('creating')}
+    e.stopPropagation();
+    return false;
+  }
+
+  onAreaCreate(x: number, y: number): Clickable {
+    const bodypart = new Clickable({x, y, width: 100, height: 100});
+    return bodypart
+  }
+
+  onAreaEdit(bodypart: Clickable): Clickable {
+    return bodypart;
+  }
+
+}
+
+export class Clickable {
+  //coords: {x: number, y: number}[] = [{x: 0, y: 0}, {x: 100, y: 0}, {x: 100, y: 100}, {x: 0, y: 100}]; 
+  //I want polygons, but I don't know how to do that right now
+  x: number = 0
+  y: number = 0
+  width: number = 100
+  height: number = 100
+  name?: string
+
+  constructor(init?: Partial<Clickable>) {
+    Object.assign(this, init);
+  }
 }
--- Frontend/CloudCure-Angular/src/app/diagnosis/diagnosis.component.ts
+++ Frontend/CloudCure-Angular/src/app/diagnosis/diagnosis.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Clickable } from '../body-clicker/body-clicker.component';
 
 @Component({
   selector: 'app-diagnosis',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiagnosisComponent implements OnInit {
 
-  constructor() { }
+  constructor(){
+    this.showBodyClicker = false;
+  }
 
-  ngOnInit(): void {
+  ngOnInit(): void{
+
+  }
+
+  showBodyClicker: boolean;
+  getClick(bodypart: Clickable) {
+    console.log(`Clicked on ${bodypart.name}`)
   }
 
 }
