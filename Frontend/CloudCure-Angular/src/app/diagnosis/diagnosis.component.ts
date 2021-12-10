75797d93df7ea3318efdf7b1987bcbf54c88918d
Brian Stiner <Brian.Stiner@revature.net>
2021-12-10 08:05:25 -0800

    transfer of bodyclicker to assesment

--- Frontend/CloudCure-Angular/src/app/diagnosis/diagnosis.component.ts
+++ Frontend/CloudCure-Angular/src/app/diagnosis/diagnosis.component.ts
@@ -9,22 +9,14 @@ import { Clickable } from '../body-clicker/body-clicker.component';
 export class DiagnosisComponent implements OnInit {
 
   //variables
-  showBodyClicker: boolean;
-  bodypartshurt: string = "";
 
   constructor(){
-    this.showBodyClicker = true;
   }
 
   ngOnInit(): void{
 
   }
 
-  getClick(bodypart: Clickable) {
-    console.log(`Clicked on ${bodypart.name}`)
-  }
-  onShowBodyClickerClicked(){
-    this.showBodyClicker = !this.showBodyClicker;
-  }
+
 
 }
