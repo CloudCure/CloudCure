0c12bf719e50b12e3c0fcc32f22a93fee607ed52
Colin Mudie <csmudie1@gmail.com>
2021-12-10 16:30:05 -0500

    added http import to app.module

--- Frontend/CloudCure-Angular/src/app/home/home.component.ts
+++ Frontend/CloudCure-Angular/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '@auth0/auth0-angular';
 import { WebApiService } from '../services/web-api.service';
@@ -10,18 +11,19 @@ import { WebApiService } from '../services/web-api.service';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private auth0:AuthService, private router:Router, private webApi: WebApiService ) 
+  constructor(public auth0: AuthService, @Inject(DOCUMENT) public document: Document, public router: Router, public webApi: WebApiService ) 
   { 
-    //checks if user is currently logged into auth0.
+    // checks if user is currently logged into auth0.
     this.auth0.user$.subscribe(
       (user) => {
         console.log(user);
         if (user)
         {
-          //search database for user.email
+          // search database for user.email
           this.webApi.loginUser(user.email).subscribe(
             (response) => 
             {
+              console.log(response);
               if (!response)
               {
                 /*If user is not in database, route to register*/
