9b6229206c034e13445607a16fe19a582a6a2805
endochroma <84640670+endochroma@users.noreply.github.com>
2021-12-10 09:18:55 -0700

    patient model

--- Backend/Models/Diagnosis/Patient.cs
+++ Backend/Models/Diagnosis/Patient.cs
@@ -10,23 +10,6 @@ namespace Models.Diagnosis
         [Key]
         public int PatientId { get; set; }
 
-        [Required]
-        [Display(Name = "Patient Name")]
-        public string PatientName { get; set; }
-
-        [Required]
-        [Display(Name = "Patient Phone")]
-        public string PatientPhone { get; set; }
-
-        [Required]
-        [Display(Name = "Patient Address")]
-        public string Address { get; set; }
-
-        [Required]
-        [Column(TypeName = "date")]
-        [DataType(DataType.Date)]
-        public DateTime DateOfBirth { get; set; }
-
         public List<Condition> Conditions { get; set; }
 
         public List<Allergy> Allergies { get; set; }
