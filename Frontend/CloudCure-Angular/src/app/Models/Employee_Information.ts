e47b52dcc518c72e0a54e39ff153c6ee277b7d35
Danny Lara <dlara2021@gmail.com>
2021-12-10 16:40:56 -0500

    Added new model

---
+++ Frontend/CloudCure-Angular/src/app/Models/Employee_Information.ts
@@ -0,0 +1,8 @@
+export interface Employee_Information {
+    user_ID: number
+    work_Email: string
+    specialization: string
+    start_Date: string
+    room_Number: string
+    education_Degree: string
+}
