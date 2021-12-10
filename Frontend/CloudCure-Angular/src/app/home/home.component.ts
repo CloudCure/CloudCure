21863cbce452e4ff45240e9c350061d34f4e1b31
ccwilki1 <ccwilki2@yahoo.com>
2021-12-10 16:03:12 -0500

    added logic to check DB for user

--- Frontend/CloudCure-Angular/src/app/home/home.component.ts
+++ Frontend/CloudCure-Angular/src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '@auth0/auth0-angular';
+import { WebApiService } from '../services/web-api.service';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth0:AuthService, private router:Router, private webApi: WebApiService ) 
+  { 
+    //checks if user is currently logged into auth0.
+    this.auth0.user$.subscribe(
+      (user) => {
+        console.log(user);
+        if (user)
+        {
+          //search database for user.email
+          this.webApi.loginUser(user.email).subscribe(
+            (response) => 
+            {
+              if (!response)
+              {
+                /*If user is not in database, route to register*/
+              }
+            }
+          )          
+        }
+      }
+    )
+  }
 
   ngOnInit(): void {
   }
