1bee934b75d3207aba8a43ef619691bc176d0a53
jerid92 <33670855+jerid92@users.noreply.github.com>
2021-12-10 15:09:48 -0600

    Added new Angular Models

--- Frontend/CloudCure-Angular/src/app/Models/User_Profile.ts
+++ Frontend/CloudCure-Angular/src/app/Models/User_Profile.ts
@@ -1,12 +1,12 @@
 export interface User_Profile {
 
-    work_Email : string
-    employee_FirstName : string
-    employee_LastName : string
+    first_Name : string
+    last_Name : string
     employee_PhoneNumber: string
-    employee_Specialization : string 
-    alternate_Email : string 
-    emergency_Contact : string 
-    user_Role : string
+    DateOfBirth: string
+    phone_Number : string 
+    address : string 
+    role_ID : number
+    user_ID : number
 }
 
