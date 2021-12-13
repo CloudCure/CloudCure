3962754ef7bbfebb63e592146a7167668524ebf8
endochroma <84640670+endochroma@users.noreply.github.com>
2021-12-13 08:33:54 -0700

    dbcontext

---
+++ Backend/Data/CloudCureDbContext.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using Models;
+using Models.Diagnosis;
+
+namespace Data
+{
+    public class CloudCureDbContext : DbContext {
+        public CloudCureDbContext(){}
+
+        public virtual DbSet<Allergy> Allergies { get; set; }
+        public virtual DbSet<Assessment> Assessments { get; set; }
+        public virtual DbSet<Condition> Conditions { get; set; }
+        public virtual DbSet<Medication> Medications { get; set; }
+        public virtual DbSet<Patient> Patients { get; set; }
+        public virtual DbSet<Surgery> Surgeries { get; set; }
+        public virtual DbSet<Vitals> Vitals { get; set; }
+        public virtual DbSet<CovidVerify> CovidVerifies { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+    }
+}
