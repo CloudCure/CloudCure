d74e1f7a4e3ef8f6d9a5c36a5bdd3ad0be5a99b9
JoshuaJL2021 <JoshuaJLopez@yahoo.com>
2021-12-14 13:19:16 -0500

    Added user controller and employeeinfo controller

--- Backend/WebAPI/EmployeeInformationController.cs
+++ Backend/WebAPI/EmployeeInformationController.cs
@@ -1,10 +1,73 @@
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Models;
+using Data;
 
-namespace WebAPI {
-    
+namespace WebAPI
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class EmployeeInformationController : ControllerBase
+    {
+        private readonly IEmployeeInformationRepository employeeRepository;
+
+        public EmployeeInformationController(IEmployeeInformationRepository context)
+        {
+            employeeRepository = context;
+        }
+
+        [HttpGet("All")]
+        public IActionResult GetAllUsers()
+        {
+
+            return null;
+        }
+
+
+        [HttpGet("GetUserById/{id}")]
+        public IActionResult GetUserById(int id)
+        {
+
+            return Ok(employeeRepository.GetByPrimaryKey(id));
+
+        }
+
+
+        [HttpGet("Verify/{email}")]
+        public IActionResult VerifyUser(string email)
+        {
+            return null;
+        }
+
+
+
+        [HttpPost("Add")]
+        public IActionResult AddEmployeeInformation([FromBody] EmployeeInformation p_user)
+        {
+            employeeRepository.Create(p_user);
+            employeeRepository.Save();
+            return Created("EmployeeInformation/Add", p_user);
+
+        }
+
+
+        [HttpPut("Update")]
+        public IActionResult UpdateUser(int id, [FromBody] EmployeeInformation p_user)
+        {
+            var item = employeeRepository.GetByPrimaryKey(id);
+            employeeRepository.Update(p_user);
+            employeeRepository.Save();
+            return null;
+        }
+
+        [HttpDelete("DeleteUser/{id}")]
+        public IActionResult DeleteUser(int id)
+        {
+            var item = employeeRepository.GetByPrimaryKey(id);
+            employeeRepository.Delete(item);
+            employeeRepository.Save();
+            return Ok();
+
+        }
+    }
 }
+
