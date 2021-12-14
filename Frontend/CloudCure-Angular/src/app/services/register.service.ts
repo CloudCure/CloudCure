60c3938873a23e05d306c5ba320d5b77a7eeef9e
AdamCzyryca <62111741+aczy623@users.noreply.github.com>
2021-12-14 12:33:15 -0500

    this should be correct untill controllers are made

--- Frontend/CloudCure-Angular/src/app/services/register.service.ts
+++ Frontend/CloudCure-Angular/src/app/services/register.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from '@auth0/auth0-angular';
+import { EmployeeInformation } from '../Models/EmployeeInformation';
 
 @Injectable({
   providedIn: 'root'
@@ -14,18 +16,18 @@ export class RegisterService {
 
   ////////////// User //////////////
   //takes the email fron auth0
-  RegisterUser(p_email:string|undefined) 
+  RegisterUser(Info:User|undefined) 
   { 
     //this route will need to match route on API controller
-    return this.http.post<any>(`${this.endpoint}/user/Register`,p_email);
+    return this.http.post<any>(`${this.endpoint}/user/Register`,Info);
   }
 
   ////////////// Employee //////////////
   //takes the id of the created user then adds the additonal info to make an employee
-  RegisterEmplyee(Id:Number|undefined) 
+  RegisterEmplyee(Info:EmployeeInformation|undefined) 
   { 
     //this route will need to match route on API controller
-    return this.http.post<any>(`${this.endpoint}/EmployeeInformation/Register`,Id);
+    return this.http.post<any>(`${this.endpoint}/EmployeeInformation/Register`,Info);
   }
 
 
