b050d7ddfb743e97a362aca586ca12c9ffd68bb2
AdamCzyryca <62111741+aczy623@users.noreply.github.com>
2021-12-14 11:58:45 -0500

    register service

--- Frontend/CloudCure-Angular/src/app/services/register.service.ts
+++ Frontend/CloudCure-Angular/src/app/services/register.service.ts
@@ -6,16 +6,25 @@ import { Injectable } from '@angular/core';
 })
 export class RegisterService {
   //endpoint will need to change to match correct endpoint
-  private endpoint: string = "";
+  private endpoint: string = "https://cloudcure-api.azurewebsites.net";
   constructor(private http:HttpClient) 
   { 
 
   }
 
   ////////////// User //////////////
+  //takes the email fron auth0
   RegisterUser(p_email:string|undefined) 
   { 
     //this route will need to match route on API controller
     return this.http.get<any>(`${this.endpoint}/user/Register/${p_email}`);
+  }
+
+  ////////////// Employee //////////////
+  //takes the id of the created user then adds the additonal info to make an employee
+  RegisterEmplyee(Id:Number|undefined) 
+  { 
+    //this route will need to match route on API controller
+    return this.http.get<any>(`${this.endpoint}/Eployee/Register/${Id}`);
   }
 }
