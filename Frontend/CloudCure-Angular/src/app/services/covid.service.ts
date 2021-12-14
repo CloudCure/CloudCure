c5c23495e5b20d2977f92bf2846ce69355aa91aa
AdamCzyryca <62111741+aczy623@users.noreply.github.com>
2021-12-14 14:18:41 -0500

    added covidVerify Service

---
+++ Frontend/CloudCure-Angular/src/app/services/covid.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { CovidVerify } from '../Models/CovidVerify';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CovidService {
+
+  private endpoint: string = "https://cloudcure-api.azurewebsites.net";
+  constructor(private http:HttpClient)
+  {
+    
+  } 
+
+  ////////////// Covid //////////////
+  GetAllCovidInfo() 
+  { 
+    //this route will need to match route on API controller
+    return this.http.get<CovidVerify[]>(`${this.endpoint}/Covid/GetAll`);
+  }
+
+  GetCovidInfoById(Id:Number | undefined) 
+  { 
+    //this route will need to match route on API controller
+    return this.http.get<CovidVerify>(`${this.endpoint}/Covid/Get/${Id}`);
+  }
+
+  AddCovidInfo(Info:CovidVerify | undefined) 
+  { 
+    //this route will need to match route on API controller
+    return this.http.post<CovidVerify>(`${this.endpoint}/Covid/Add`,Info);
+  }  
+
+  DeleteCovidInfo(Info:CovidVerify | undefined) 
+  { 
+    //this route will need to match route on API controller
+    return this.http.post<CovidVerify>(`${this.endpoint}/Covid/Add`,Info);
+  }  
+}
