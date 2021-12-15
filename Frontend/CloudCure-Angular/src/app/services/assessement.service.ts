edcacaed28e9522b01542f063ca192c49ccfb53d
Brian Stiner <Brian.Stiner@revature.net>
2021-12-15 09:36:30 -0800

    assessement service created

--- Frontend/CloudCure-Angular/src/app/services/assessement.service.ts
+++ Frontend/CloudCure-Angular/src/app/services/assessement.service.ts
@@ -1,9 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Assessment } from '../AngularModels/Assessment';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AssessementService {
+export class AssessmentService {
 
-  constructor() { }
+  private endpoint: string = "https://cloudcure-api.azurewebsites.net/api/Assessment";
+  constructor(private http:HttpClient) {} 
+
+  ////////////// Assessment //////////////
+  GetAll(){ 
+    return this.http.get<Assessment[]>(`${this.endpoint}/Get/All`);
+  }
+
+  GetById(Id:Number | undefined){ 
+    return this.http.get<Assessment>(`${this.endpoint}/Get/${Id}`);
+  }
+
+  Add(Info:Assessment | undefined){ 
+    return this.http.post<Assessment>(`${this.endpoint}/Add`,Info);
+  }  
+
+  Delete(Id:number| undefined){ 
+    return this.http.delete<Assessment>(`${this.endpoint}/Delete/${Id}`);
+  }  
+
+  Update(Id:number| undefined, Info:Assessment | undefined){
+    return this.http.put<Assessment>(`${this.endpoint}/Update/${Id}`,Info);
+  }
+
 }
+
