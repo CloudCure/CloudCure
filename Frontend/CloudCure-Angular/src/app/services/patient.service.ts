505a827a3a42103121d5e10e047675b9820a8ed7
AdamCzyryca <62111741+aczy623@users.noreply.github.com>
2021-12-15 12:07:12 -0500

    start of patient service

---
+++ Frontend/CloudCure-Angular/src/app/services/patient.service.ts
@@ -0,0 +1,86 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Allgergy } from '../AngularModels/Allergy';
+import { Condition } from '../AngularModels/Condition';
+import { Medication } from '../AngularModels/Medication';
+import { Patient } from '../AngularModels/Patient';
+import { Surgery } from '../AngularModels/Surgery';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PatientService {
+
+  private endpoint: string = "https://cloudcure-api.azurewebsites.net/api/Patient";
+  constructor(private http:HttpClient) {} 
+
+  ////////////// Patient //////////////
+  GetAll(){ 
+    return this.http.get<Patient[]>(`${this.endpoint}/Get/All`);
+  }
+
+  GetAllAllergies(){ 
+    return this.http.get<Allgergy[]>(`${this.endpoint}/Get/All/Allergies`);
+  }
+
+  GetAllConditions(){ 
+    return this.http.get<Condition[]>(`${this.endpoint}/Get/All/Conditions`);
+  }
+
+  GetAllMedications(){ 
+    return this.http.get<Medication[]>(`${this.endpoint}/Get/All/Medications`);
+  }
+
+  GetAllSurgeries(){ 
+    return this.http.get<Surgery[]>(`${this.endpoint}/Get/All/Surgeries`);
+  }
+
+  GetById(Id:Number | undefined){ 
+    return this.http.get<Patient>(`${this.endpoint}/Get/${Id}`);
+  }
+
+  Add(Info:Patient | undefined){ 
+    return this.http.post<Patient>(`${this.endpoint}/Add`,Info);
+  }  
+
+  AddAllgergy(Info:Allgergy | undefined){ 
+    return this.http.post<Allgergy>(`${this.endpoint}/Add/Allgergy`,Info);
+  }  
+
+  AddCondition(Info:Condition | undefined){ 
+    return this.http.post<Condition>(`${this.endpoint}/Add/Condition`,Info);
+  }  
+
+  AddMedication(Info:Medication | undefined){ 
+    return this.http.post<Medication>(`${this.endpoint}/Add/Medication`,Info);
+  }  
+
+  AddSurgery(Info:Surgery | undefined){ 
+    return this.http.post<Surgery>(`${this.endpoint}/Add/Surgery`,Info);
+  }  
+
+  Delete(Id:number| undefined){ 
+    return this.http.delete<Patient>(`${this.endpoint}/Delete/${Id}`);
+  }  
+
+  DeleteAllgergy(Id:number| undefined){ 
+    return this.http.delete<Allgergy>(`${this.endpoint}/Delete/Allgergy/${Id}`);
+  } 
+
+  DeleteCondition(Id:number| undefined){ 
+    return this.http.delete<Condition>(`${this.endpoint}/Delete/Condition/${Id}`);
+  } 
+
+  DeleteMedication(Id:number| undefined){ 
+    return this.http.delete<Medication>(`${this.endpoint}/Delete/${Id}`);
+  } 
+
+  DeleteSurgery(Id:number| undefined){ 
+    return this.http.delete<Surgery>(`${this.endpoint}/Delete/Surgery/${Id}`);
+  } 
+
+  Update(Id:number| undefined, Info:Patient | undefined){
+    return this.http.put<Patient>(`${this.endpoint}/Update/${Id}`,Info);
+  }
+
+}
--- Frontend/CloudCure-Angular/src/app/services/vitals.service.ts
+++ Frontend/CloudCure-Angular/src/app/services/vitals.service.ts
@@ -7,38 +7,38 @@ import { Vitals } from '../AngularModels/Vitals';
 })
 export class VitalsService {
 
-  private endpoint: string = "https://cloudcure-api.azurewebsites.net";
+  private endpoint: string = "https://cloudcure-api.azurewebsites.net/api/Vitals";
   constructor(public http:HttpClient) { }
 
   ////////////// Vitals //////////////
   GetAll() 
   { 
     //this route will need to match route on API controller
-    return this.http.get<Vitals[]>(`${this.endpoint}/api/Vitals/All`);
+    return this.http.get<Vitals[]>(`${this.endpoint}/All`);
   }
 
   GetById(Id:Number | undefined) 
   { 
     //this route will need to match route on API controller
-    return this.http.get<Vitals>(`${this.endpoint}/api/Vitals/${Id}`);
+    return this.http.get<Vitals>(`${this.endpoint}/${Id}`);
   }
 
   Add(Info:Vitals | undefined) 
   { 
     //this route will need to match route on API controller
-    return this.http.post<Vitals>(`${this.endpoint}/api/Vitals/Add`,Info);
+    return this.http.post<Vitals>(`${this.endpoint}/Add`,Info);
   }  
 
   update(Info:Vitals | undefined, Id:Number | undefined) 
   { 
     //this route will need to match route on API controller
-    return this.http.put<Vitals>(`${this.endpoint}/api/Vitals/update/${Id}`,Info);
+    return this.http.put<Vitals>(`${this.endpoint}/update/${Id}`,Info);
   } 
 
   Delete(Id:number | undefined) 
   { 
     //this route will need to match route on API controller
-    return this.http.delete<Vitals>(`${this.endpoint}/api/Vitals/delete/${Id}`);
+    return this.http.delete<Vitals>(`${this.endpoint}/delete/${Id}`);
   } 
 
 }
