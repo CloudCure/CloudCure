4add6b1078e13742fe81e1fbbe0463eceef7dab3
Brian Stiner <Brian.Stiner@revature.net>
2021-12-15 11:58:28 -0800

    fixed up allergycontroller

--- Backend/WebAPI/Controllers/AssessmentsController.cs
+++ Backend/WebAPI/Controllers/AssessmentsController.cs
@@ -6,7 +6,7 @@ using Serilog;
 
 namespace WebAPI.Controllers
 {
-    [Route("api/[Controller]")]
+    [Route("assessment")]
     [ApiController]
     public class AssessmentController : ControllerBase
     {
@@ -16,84 +16,64 @@ namespace WebAPI.Controllers
 
         // GET: api/Assessment/Get/All
         [HttpGet("Get/All")]
-        public IActionResult GetAll()
-        {
-            try
-            {
+        public IActionResult GetAll(){
+            try{
                 return Ok(_repo.GetAll());
-            }
-            catch (Exception e)
-            {
+            }catch (Exception e){
                 Log.Error(e.Message);
-                return BadRequest("Invalid get all request.");
+                return BadRequest("Invalid get all Assessment request.");
             }
         }
 
         // GET api/Assessment/Get/{p_id}
         [HttpGet("Get/{id}")]
-        public IActionResult GetByPrimaryKey(int p_id)
-        {
-            try
-            {
+        public IActionResult GetByPrimaryKey(int p_id){
+            try{
                 return Ok(_repo.GetByPrimaryKey(p_id));
-            }
-            catch (Exception e)
-            {
+            }catch (Exception e){
                 Log.Error(e.Message);
-                return BadRequest("Not a valid ID");
+                return BadRequest("Invalid Assessment Get request.");
             }
         }
 
         //POST api/Assessment/add 
         [HttpPost("Add")]
-        public IActionResult Add([FromBody] Assessment p_Assessment)
-        { // Fixme: This is not working
-            try
-            {
+        public IActionResult Add([FromBody] Assessment p_Assessment){ 
+            try{
                 _repo.Create(p_Assessment);
                 _repo.Save();
                 return Ok();
-            }
-            catch (Exception e)
-            {
+            }catch (Exception e){
                 Log.Error(e.Message);
-                return BadRequest("Invalid input.");
+                return BadRequest("Invalid Assessment Add Request.");
             }
         }
 
         // DELETE api/delete/{p_id}
         [HttpDelete("Delete/{p_id}")]
-        public IActionResult Delete(int p_id)
-        {
-            try
-            {
+        public IActionResult Delete(int p_id){
+            try{
                 var topic = _repo.GetByPrimaryKey(p_id);
                 _repo.Delete(topic);
                 _repo.Save();
                 return Ok();
-            }
-            catch (Exception e)
-            {
+            }catch (Exception e){
                 Log.Error(e.Message);
-                return BadRequest("Not a valid Id");
+                return BadRequest("Invalid Assessment Delete request.");
             }
         }
 
         // PUT api/Assessment/update/{id}
         [HttpPut("Update/{id}")]
-        public IActionResult Update(int id, [FromBody] Assessment p_Assessment)
-        { // Fixme: This is not working
-            try
-            {
+        public IActionResult Update(int id, [FromBody] Assessment p_Assessment){ 
+            try{
                 p_Assessment.Id = id;
                 _repo.Update(p_Assessment);
                 _repo.Save();
                 return Ok();
-            }
-            catch (Exception e)
-            {
+            }catch (Exception e){
                 Log.Error(e.Message);
-                return BadRequest("Invalid Input");
+                return BadRequest("Invalid Assessments Update");
             }
         }
     }
