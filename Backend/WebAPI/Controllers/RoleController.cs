ebcdc08e26ad82db089a46742ee77f98019b3c4e
sarahbethd <sarahbethd@gmail.com>
2021-12-15 14:19:17 -0500

    Role and Vitals Try/Catch

--- Backend/WebAPI/Controllers/RoleController.cs
+++ Backend/WebAPI/Controllers/RoleController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Models;
 using Data;
+using Serilog;
 
 namespace WebAPI.Controllers
 {
@@ -22,45 +23,86 @@ namespace WebAPI.Controllers
         }
 
         // GET: Role/All
-        [HttpGet("All")] //("All") Will give and endpoint that ends with All
+        [HttpGet("All")] //("All") Will give an endpoint that ends with All
         public IActionResult GetAllRole()
-        {
+        {   
+            try
+            {
             return Ok(_repo.GetAll());
+            }
+            catch (Exception e)
+            {
+                Log.Error(e.Message);
+                return BadRequest("Invalid get all request.");
+            }
         }
 
         // GET Role/Id
-        [HttpGet("{p_id}")]
+        [HttpGet("{p_id}")] //("ID") Will give an endpoint that ends with Id
         public IActionResult GetRoleById(int p_id)
         {
-            return Ok(_repo.GetByPrimaryKey(p_id));
+            try
+            {
+                return Ok(_repo.GetByPrimaryKey(p_id));
+            }
+            catch (Exception e)
+            {
+                Log.Error(e.Message);
+                return BadRequest("Invalid ID");
+            }
         }
-
         // POST Role/Add
-        [HttpPost("add")]
+        [HttpPost("add")]//("Add") Will give an endpoint that ends with Add
         public IActionResult AddRole([FromBody] Role p_role)
         {
+            try
+            {
             _repo.Create(p_role);
             _repo.Save();
-            return Created("Role/add", p_role);
+            return Created("api/covid/add", p_role);
+            }
+            catch (Exception e)
+            {
+                Log.Error(e.Message);
+                return BadRequest("Invalid Input");
+            }
         }
 
 
         // PUT Role/Edit
-        [HttpPut("edit/{id}")]
+        [HttpPut("edit/{id}")]//("ID") Will give an endpoint that ends with Id
         public IActionResult UpdateRole([FromBody] Role p_role)
         {
-            _repo.Update(p_role);
-            _repo.Save();
-            return Ok();
+            try
+            {
+                _repo.Update(p_role);
+                _repo.Save();
+                return Ok();
+            }
+            catch (Exception e)
+            {
+
+                Log.Error(e.Message);
+                return BadRequest("Invalid ID");
+            }
         }
 
         // DELETE Role/Id
-        [HttpDelete("delete/{id}")]
+        [HttpDelete("delete/{id}")]//("ID") Will give an endpoint that ends with Id
         public IActionResult DeleteRole([FromBody] Role p_role)
         {
-            _repo.Delete(p_role);
-            _repo.Save();
-            return Ok();
+            try
+            {
+                _repo.Delete(p_role);
+                _repo.Save();
+                return Ok();
+            }
+            catch (Exception e)
+            {
+
+                Log.Error(e.Message);
+                return BadRequest("Invalid ID");
+            }
         }
     }
 }
--- Backend/WebAPI/Controllers/VitalsController.cs
+++ Backend/WebAPI/Controllers/VitalsController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Data;
 using Microsoft.AspNetCore.Mvc;
 using Models.Diagnosis;
+using Serilog;
 
 namespace WebAPI.Controllers
 {
@@ -21,44 +22,83 @@ namespace WebAPI.Controllers
         }
 
         //GET: Vitals/All
-        [HttpGet("All")] //("All") Will give and endpoint that ends with All
+        [HttpGet("All")] //("All") Will give an endpoint that ends with All
         public IActionResult GetAllVitals()
-        {
+        {   
+            try
+            {
             return Ok(_repo.GetAll());
+            }
+            catch (Exception e)
+            {
+                Log.Error(e.Message);
+                return BadRequest("Invalid get all request");
+            }
         }
 
         //GET: Vitals/Id
-        [HttpGet("{p_id}")]
+        [HttpGet("{p_id}")]//("ID") Will give an endpoint that ends with Id
         public IActionResult GetVitalsById(int p_id)
         {
-            return Ok(_repo.GetByPrimaryKey(p_id));
+            try
+            {
+                return Ok(_repo.GetByPrimaryKey(p_id));
+            }
+            catch (Exception e)
+            {
+                Log.Error(e.Message);
+                return BadRequest("Invalid ID");
+            }
         }
-
         //POST: Vitals/Add
-        [HttpPost("add")]
+        [HttpPost("add")]//("Add") Will give an endpoint that ends with Add
         public IActionResult AddVitals([FromBody] Vitals p_vitals)
-        {
-            _repo.Create(p_vitals);
-            _repo.Save();
-            return Created("Vitals/add", p_vitals);
+         {
+            try
+            {
+                _repo.Create(p_vitals);
+                _repo.Save();
+                return Created("vitals/add", p_vitals);
+            }
+            catch (Exception e)
+            {
+                Log.Error(e.Message);
+                return BadRequest("Invalid Input");
+            }
         }
-
         //PUT: Vitals/Id
-        [HttpPut("edit/{id}")]
+        [HttpPut("edit/{id}")]//("ID") Will give an endpoint that ends with Id
         public IActionResult UpdateVitals([FromBody] Vitals p_vitals)
         {
-            _repo.Update(p_vitals);
-            _repo.Save();
-            return Ok();
+            try
+            {
+                _repo.Update(p_vitals);
+                _repo.Save();
+                return Ok();
+            }
+            catch (Exception e)
+            {
+
+                Log.Error(e.Message);
+                return BadRequest("Invalid ID");
+            }
         }
 
         //Delete: Vitals/Id
-        [HttpDelete("delete/{id}")]
+        [HttpDelete("delete/{id}")]//("ID") Will give an endpoint that ends with Id
         public IActionResult DeleteVitals([FromBody] Vitals p_vitals)
         {
-            _repo.Delete(p_vitals);
-            _repo.Save();
-            return Ok();
+            try
+            {
+                _repo.Delete(p_vitals);
+                _repo.Save();
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                Log.Error(e.Message);
+                return BadRequest("Invalid ID");
+            }
         }
     }
 }
