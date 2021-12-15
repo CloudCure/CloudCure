af0b32917a347336b5674e5db9858ac028729534
Brian Stiner <Brian.Stiner@revature.net>
2021-12-15 12:40:23 -0800

    Allergy service

--- Frontend/CloudCure-Angular/src/app/services/allergy.service.ts
+++ Frontend/CloudCure-Angular/src/app/services/allergy.service.ts
@@ -1,9 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Allergy } from '../AngularModels/Allergy';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AllergyService {
 
-  constructor() { }
+  private endpoint: string = "https://cloudcure-api.azurewebsites.net/api/Allergy";
+  constructor(private http:HttpClient) {} 
+
+  ////////////// Allergy //////////////
+
+
+  GetAll(){ 
+    return this.http.get<Allergy[]>(`${this.endpoint}/Get/All`);
+  }
+
+  GetById(Id:Number | undefined){ 
+    return this.http.get<Allergy>(`${this.endpoint}/Get/${Id}`);
+  }
+
+  Add(Info:Allergy | undefined){ 
+    return this.http.post<Allergy>(`${this.endpoint}/Add`,Info);
+  }  
+
+  Delete(Id:number| undefined){ 
+    return this.http.delete<Allergy>(`${this.endpoint}/Delete/${Id}`);
+  }  
+
+  Update(Id:number| undefined, Info:Allergy | undefined){
+    return this.http.put<Allergy>(`${this.endpoint}/Update/${Id}`,Info);
+  }
 }
