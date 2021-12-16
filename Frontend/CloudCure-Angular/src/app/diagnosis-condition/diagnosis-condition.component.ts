39980cb5c515459fd7fdf062f143ef5df17d4d96
AdamCzyryca <62111741+aczy623@users.noreply.github.com>
2021-12-16 15:03:11 -0500

    condition component TS

--- Frontend/CloudCure-Angular/src/app/diagnosis-condition/diagnosis-condition.component.ts
+++ Frontend/CloudCure-Angular/src/app/diagnosis-condition/diagnosis-condition.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Condition } from '../AngularModels/Condition';
+import { ConditionService } from '../services/condition.service';
 
 @Component({
   selector: 'diagnosis-condition',
@@ -11,11 +14,30 @@ export class DiagnosisConditionComponent implements OnInit {
   display: boolean = false;
   conditions: string[] = [''];
 
-  constructor() { }
+  ConditionGroup: FormGroup = new FormGroup({
+    PatientId: new FormControl(2,Validators.required),//is hardcoded to patient 2 this will change once it is connected to the diagnosis component
+    ConditionName: new FormControl("",Validators.required),
+  })
+
+  constructor(private Condition: Condition, private ConditionApi: ConditionService) { }
 
   ngOnInit(): void {
   }
 
+  AddCondition(ConditionGroup: FormGroup){
+    if(ConditionGroup.valid)
+    {
+      let ConditionInfo: Condition = {
+        PatientId: ConditionGroup.get("PatientId")?.value,
+        ConditionName: ConditionGroup.get("ConditionName")?.value,
+      }
 
+      this.ConditionApi.Add(ConditionInfo).subscribe(
+          (response) => {
+            console.log("Condition added");
+            console.log(response);
+          })
+    }
+  }
 
 }
