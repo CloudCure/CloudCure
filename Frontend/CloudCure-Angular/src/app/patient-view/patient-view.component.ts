b11d1abfd4ba158de09cea3ccf5f57d0be52b324
AnddyLopez <anddylopez5324@gmail.com>
2021-12-17 15:09:04 -0600

    Added patient view component

---
+++ Frontend/CloudCure-Angular/src/app/patient-view/patient-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-patient-view',
+  templateUrl: './patient-view.component.html',
+  styleUrls: ['./patient-view.component.css']
+})
+export class PatientViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
