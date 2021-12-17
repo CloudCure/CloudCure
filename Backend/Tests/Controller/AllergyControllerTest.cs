96930f9e5e90088b644196e8b7878f22edfb1105
ocrowley1233 <oisin.p.crowley@gmail.com>
2021-12-17 11:05:59 -0500

    allergy controller test

--- Backend/Tests/Controller/AllergyControllerTest.cs
+++ Backend/Tests/Controller/AllergyControllerTest.cs
@@ -19,7 +19,7 @@ namespace Tests
             var repository = new Mock<IAllergyRepository>();
             var controller = new AllergyController(repository.Object);
 
-            var allergy = new Allergy 
+            var allergy = new Allergy
             {
                 PatientId = 1,
                 AllergyName = "Hay fever"
@@ -29,17 +29,79 @@ namespace Tests
             var okResponse = (IStatusCodeActionResult)result;
             Assert.Equal(201, okResponse.StatusCode);
         }
-       
 
-      private List<Allergy> GetAllergyList()
-      {
-          var testAllergy = new List<Allergy>();
-          testAllergy.Add(new Allergy {Id = 1, AllergyName = "Meds", PatientId = 1});
-          testAllergy.Add(new Allergy {Id = 2, AllergyName = "Dogs", PatientId = 2});
-          testAllergy.Add(new Allergy {Id = 3, AllergyName = "Cats", PatientId = 3});
+        [Fact]
+        public void GetAllShouldGetAll()
+        {
+            var repository = new Mock<IAllergyRepository>();
+            var controller = new AllergyController(repository.Object);
 
-          return testAllergy;
-      }
-       
+            var allergy = new Allergy
+            {
+                PatientId = 1,
+                AllergyName = "Hay fever"
+            };
+
+            var entry = controller.Add(allergy);
+            var result = controller.GetAll();
+            var okResponse = (IStatusCodeActionResult)result;
+            Assert.Equal(200, okResponse.StatusCode);
+
+        }
+
+        [Fact]
+        public void DeleteShouldDeleteEntry()
+        {
+            var repository = new Mock<IAllergyRepository>();
+            var controller = new AllergyController(repository.Object);
+
+            var allergy = new Allergy
+            {
+                PatientId = 1,
+                AllergyName = "Hay fever"
+            };
+
+            var entry = controller.Add(allergy);
+
+            var result = controller.Delete(allergy);
+            var okResponse = (IStatusCodeActionResult)result;
+            Assert.Equal(200, okResponse.StatusCode);
+
+        }
+
+        [Fact]
+        public void UpdateShouldUpdateAllergy()
+        {
+            var repository = new Mock<IAllergyRepository>();
+            var controller = new AllergyController(repository.Object);
+
+            var allergy = new Allergy
+            {
+                PatientId = 1,
+                AllergyName = "Hay fever"
+            };
+
+            var entry = controller.Add(allergy);
+            var result = controller.Update(1 , allergy);
+            var okResponse = (IStatusCodeActionResult)result;
+            Assert.Equal(200, okResponse.StatusCode);
+            
+
+
+
+            
+        }
+
+
+        private List<Allergy> GetAllergyList()
+        {
+            var testAllergy = new List<Allergy>();
+            testAllergy.Add(new Allergy { Id = 1, AllergyName = "Meds", PatientId = 1 });
+            testAllergy.Add(new Allergy { Id = 2, AllergyName = "Dogs", PatientId = 2 });
+            testAllergy.Add(new Allergy { Id = 3, AllergyName = "Cats", PatientId = 3 });
+
+            return testAllergy;
+        }
+
     }
 }
