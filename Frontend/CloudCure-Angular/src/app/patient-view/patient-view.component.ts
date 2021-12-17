5762e85e10a843e8bb4982b917e5dd70b656934b
jerid92 <33670855+jerid92@users.noreply.github.com>
2021-12-17 16:03:59 -0600

    Added Patient View componet code

--- Frontend/CloudCure-Angular/src/app/patient-view/patient-view.component.ts
+++ Frontend/CloudCure-Angular/src/app/patient-view/patient-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PatientService } from '../services/patient.service';
 
 @Component({
   selector: 'app-patient-view',
@@ -7,7 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PatientViewComponent implements OnInit {
 
-  constructor() { }
+  patientExists: Boolean = true;
+  
+  PatientId:          any;
+  PatientName:        string | undefined = 'Timmy';
+  PatientPhone:       any = '';
+  PatientAddress:     any = '';
+  DateOfBirth:        any = '';
+  Conditions:         any = [];
+  Allergies:          any = [];
+  Surgeries:          any = [];
+  CurrentMedications: any = [];
+  VitalHistory:       any = [];
+  Assessments:        any = [];
+
+  constructor(private patientApi: PatientService) {
+    //1 will be changed later
+    this.patientApi.GetById(1).subscribe(response => {
+      this.patientExists = true;
+
+      this.PatientName = response.PatientName
+      this.PatientPhone = response.PatientPhone
+      this.PatientAddress = response.PatientAddress
+      this.DateOfBirth = response.DateOfBirth
+      this.Conditions = response.Conditions
+
+    })
+   }
 
   ngOnInit(): void {
   }
