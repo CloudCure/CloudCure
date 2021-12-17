df1213de03abb333c73446cf1e7bf2880697f38c
Michael Mason <mlm@michael-mason.com>
2021-12-17 10:52:29 -0700

    tests

---
+++ Backend/Tests/Controller/PatientControllerTests.cs
@@ -0,0 +1,168 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Data;
+using Moq;
+using Xunit;
+using WebAPI.Controllers;
+using Models.Diagnosis;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Models;
+
+namespace Tests
+{
+    public class PatientControllerTests
+    {
+        [Fact]
+        public void CreateReturnsOk()
+        {
+            var repository = new Mock<IPatientRepository>();
+            var repository2 = new Mock<IAllergyRepository>();
+            var controller = new PatientController(repository.Object, repository2.Object);
+
+            var patient = newPatient();
+
+            var result = controller.Add(patient);
+            var okResponse = (IStatusCodeActionResult)result;
+            Assert.Equal(201, okResponse.StatusCode);
+        }
+
+        [Fact]
+        public void GetAllShouldGetAll()
+        {
+            var repository = new Mock<IPatientRepository>();
+            var repository2 = new Mock<IAllergyRepository>();
+            var controller = new PatientController(repository.Object, repository2.Object);
+
+            var patient = newPatient();
+
+            var entry = controller.Add(patient);
+            var result = controller.GetAll();
+            var okResponse = (IStatusCodeActionResult)result;
+            Assert.Equal(200, okResponse.StatusCode);
+        }
+
+        [Fact]
+        public void DeleteShouldDeleteEntry()
+        {
+            var repository = new Mock<IPatientRepository>();
+            var repository2 = new Mock<IAllergyRepository>();
+            var controller = new PatientController(repository.Object, repository2.Object);
+
+            var patient = newPatient();
+
+            var entry = controller.Add(patient);
+            var result = controller.Delete(patient);
+            var okResponse = (IStatusCodeActionResult)result;
+            Assert.Equal(200, okResponse.StatusCode);
+        }
+
+        [Fact]
+        public void UpdateShouldUpdatePatient()
+        {
+            var repository = new Mock<IPatientRepository>();
+            var repository2 = new Mock<IAllergyRepository>();
+            var controller = new PatientController(repository.Object, repository2.Object);
+
+            var patient = newPatient();
+
+            var entry = controller.Add(patient);
+            var result = controller.Update(1, patient);
+            var okResponse = (IStatusCodeActionResult)result;
+            Assert.Equal(200, okResponse.StatusCode);
+        }
+
+        [Fact]
+        public void GetByIdShouldGetPatientById()
+        {
+            var repository = new Mock<IPatientRepository>();
+            var repository2 = new Mock<IAllergyRepository>();
+            var controller = new PatientController(repository.Object, repository2.Object);
+
+            var patient = newPatient();
+
+            var entry = controller.Add(patient);
+            var result = controller.GetById(1);
+            var okResponse = (IStatusCodeActionResult)result;
+            Assert.Equal(200, okResponse.StatusCode);
+        }
+
+        private Patient newPatient()
+        {
+            var patient = new Patient
+            {
+                UserProfile = new User
+                {
+
+                    FirstName = "dldfk",
+                    LastName = "sdfksdf",
+                    PhoneNumber = "dkfadl",
+                    Address = "dkfjskld",
+                    DateOfBirth = DateTime.Now,
+                    EmergencyName = "dfdsfsdf",
+                    EmergencyContactPhone = "fdksfdsd",
+                    RoleId = 1
+
+                },
+                Conditions = new List<Condition>()
+                {
+                    new Condition{
+                        PatientId = 1,
+                        ConditionName = "dddf",
+
+                    }
+                },
+                Allergies = new List<Allergy>()
+                {
+                    new Allergy{
+                        AllergyName = "dddfd",
+                        PatientId = 1
+                    }
+                },
+                Surgeries = new List<Surgery>()
+                {
+                    new Surgery{
+                        PatientId = 1,
+                        SurgeryName = "dfkjdf"
+                    }
+                },
+                CurrentMedications = new List<Medication>()
+                {
+                    new Medication{
+                        PatientId = 1,
+                        MedicationName = "dfdsf"
+                    }
+                },
+                VitalHistory = new List<Vitals>()
+                {
+                    new Vitals{
+                        PatientId = 1,
+                        Diastolic = 70,
+                        Systolic = 120,
+                        HeartRate = 70,
+                        Height = 75,
+                        Weight = 200,
+                        OxygenSat = 98.2,
+                        Tempature = 97.2,
+                        RespiratoryRate = 14,
+                        EncounterDate = DateTime.Now
+
+                    }
+                },
+                Assessments = new List<Assessment>()
+                {
+                    new Assessment{
+                        PatientId = 2,
+                        PainAssessment = "asdfas",
+                        PainScale = 2,
+                        ChiefComplaint = "dfdssdf",
+                        HistoryOfPresentIllness = "dssdfs"
+                    }
+                }
+            };
+
+            return patient;
+        }
+    }
+}
