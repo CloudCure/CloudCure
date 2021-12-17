180ce4457ac684a6950f68b9e7d5907c7cc1e61f
Danny Lara <dlara2021@gmail.com>
2021-12-17 12:59:03 -0500

    Added RoleController tests

---
+++ Backend/Tests/Controller/RoleControllerTests.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Data;
+using Moq;
+using Xunit;
+using WebAPI.Controllers;
+using Models;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace Tests
+{
+    public class RoleControllerTests
+    {
+        [Fact]
+        public void CreateReturnsOk()
+        {
+            var repository = new Mock<IRoleRepository>();
+            var controller = new RoleController(repository.Object);
+
+            var role = new Role
+            {
+                RoleName = "Doctor"
+            };
+
+            var result = controller.Add(role);
+            var okResponse = (IStatusCodeActionResult)result;
+            Assert.Equal(201, okResponse.StatusCode);
+        }
+
+        [Fact]
+        public void GetAllShouldGetAll()
+        {
+            var repository = new Mock<IRoleRepository>();
+            var controller = new RoleController(repository.Object);
+
+            var role = new Role
+            {
+                RoleName = "Doctor"
+            };
+
+            var entry = controller.Add(role);
+            var result = controller.GetAll();
+            var okResponse = (IStatusCodeActionResult)result;
+            Assert.Equal(200, okResponse.StatusCode);
+        }
+
+        [Fact]
+        public void DeleteShouldDeleteEntry()
+        {
+            var repository = new Mock<IRoleRepository>();
+            var controller = new RoleController(repository.Object);
+
+            var role = new Role
+            {
+                RoleName = "Doctor"
+            };
+
+            var entry = controller.Add(role);
+            var result = controller.Delete(role);
+            var okResponse = (IStatusCodeActionResult)result;
+            Assert.Equal(200, okResponse.StatusCode);
+        }
+
+        [Fact]
+        public void UpdateShouldUpdateRole()
+        {
+            var repository = new Mock<IRoleRepository>();
+            var controller = new RoleController(repository.Object);
+
+            var role = new Role
+            {
+                RoleName = "Doctor"
+            };
+
+            var entry = controller.Add(role);
+            role.RoleName = "Nurse";
+            var result = controller.Update(1, role);
+            var okResponse = (IStatusCodeActionResult)result;
+            Assert.Equal(200, okResponse.StatusCode);
+        }
+
+        [Fact]
+        public void GetByIdShouldGetRoleById()
+        {
+            var repository = new Mock<IRoleRepository>();
+            var controller = new RoleController(repository.Object);
+
+            var role = new Role
+            {
+                RoleName = "Doctor"
+            };
+
+            var entry = controller.Add(role);
+            var result = controller.GetById(1);
+            var okResponse = (IStatusCodeActionResult)result;
+            Assert.Equal(200, okResponse.StatusCode);
+        }
+    }
+}
