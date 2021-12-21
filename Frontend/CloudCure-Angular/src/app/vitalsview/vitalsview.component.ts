ed4d81862effc1e7111a9c90c1213b3501171f58
jerid92 <33670855+jerid92@users.noreply.github.com>
2021-12-21 10:09:29 -0600

    Edited Vitalsview component

--- Frontend/CloudCure-Angular/src/app/vitalsview/vitalsview.component.ts
+++ Frontend/CloudCure-Angular/src/app/vitalsview/vitalsview.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { VitalsService } from '../services/vitals.service';
 
 @Component({
   selector: 'app-vitalsview',
@@ -7,7 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VitalsviewComponent implements OnInit {
 
-  constructor() { }
+  //vitalsExists: Boolean = true;
+  VitalsId:       any = '';
+  PatientId:       any = '';
+  Systolic:        any = '';
+  Diastolic:       any = '';
+  OxygenSat:       any = '';
+  HeartRate:       any = '';
+  Tempature:     any = '';
+  RespiratoryRate: any = '';
+  Height:          any = '';
+  Weight:          any = '';
+  EncounterDate:  any = '';
+
+  constructor(private vitalsApi: VitalsService) {
+    this.vitalsApi.GetById(1).subscribe(response => {
+      console.log("accessed vitals")
+      console.log(response)
+      //this.vitalsExists = true;
+      //Instantiating Vitals Variables
+      this.PatientId =        response.PatientId
+      this.Systolic =          this.Systolic
+      this.Diastolic =           this.Diastolic
+      this.OxygenSat =        this.OxygenSat
+      this.HeartRate =       this.HeartRate
+      this.Tempature =     this.Tempature
+      this.RespiratoryRate =        this.RespiratoryRate
+      this.Height =         response.Height
+      this.Weight =          response.Weight
+      this.EncounterDate =          response.EncounterDate
+
+    })
+   }
 
   ngOnInit(): void {
   }
