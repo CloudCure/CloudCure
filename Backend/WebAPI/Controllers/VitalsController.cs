02610c7b3d67b647373484f7765dbbe25f0e462e
Danny Lara <dlara2021@gmail.com>
2021-12-23 15:55:10 -0500

    Minor change for uniformity

--- Backend/WebAPI/Controllers/VitalsController.cs
+++ Backend/WebAPI/Controllers/VitalsController.cs
@@ -96,6 +96,7 @@ namespace WebAPI.Controllers
             {
                 if (_repo.GetById(id) == null || p_vitals == null)
                     throw new Exception("Update failed!");
+                p_vitals.Id = id;
                 _repo.Update(p_vitals);
                 _repo.Save();
                 return Ok();
